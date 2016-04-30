91bcd4487bcc8505cdb4a6010d19370bac649877
Loic Descotte <loic.descotte@gmail.com>
2016-04-30 13:28:14 +0200

    update tests

--- src/test/scala/HListSpec.scala
+++ src/test/scala/HListSpec.scala
@@ -1,5 +1,5 @@
+import io.github.hamsters.{HList, HCons, HNil}
 import org.scalatest.{FlatSpec, Matchers}
-import io.github.hamsters.{HCons, HNil}
 
 import scala.collection.mutable.ListBuffer
 
@@ -10,26 +10,48 @@ class HListSpec extends FlatSpec with Matchers {
     ("hi" :: HNil).tail shouldBe HNil
     ("hi" :: HNil).head shouldBe "hi"
 
-    //type erasure, but compiler give all the good types
-    (true :: 2.0 :: "hi" :: HNil) shouldBe a [HCons[_,HCons[_,HNil]]]
-    (true :: 2.0 :: "hi" :: HNil).tail shouldBe a [HCons[_,HNil]]
+    val hlist = true :: 2.0 :: "hi" :: HNil
+    //type erasure
+    hlist shouldBe a[HCons[_, HCons[_, HNil]]]
+    hlist.tail shouldBe a[HCons[_, HNil]]
 
-    (true :: 2.0 :: "hi" :: HNil).head shouldBe a [java.lang.Boolean]
-    (true :: 2.0 :: "hi" :: HNil).tail.head shouldBe a [java.lang.Double]
+    hlist.head shouldBe a[java.lang.Boolean]
+    hlist.tail.head shouldBe a[java.lang.Double]
 
   }
 
 
   "HList ++ " should "append hlist to another one" in {
 
-    val sum = (2.0 :: "hi" :: HNil) ++ (1 :: HNil)
+    import HList._
+
+    val hlist1 = 2.0 :: "hi" :: HNil
+    val hlist2 = 1 :: HNil
+
+    val sum = ++(hlist1, hlist2)
+
     sum shouldBe 2.0 :: "hi" :: 1 :: HNil
-    sum shouldBe a [HCons[_,HCons[_,HCons[_,HNil]]]]
-    sum.head shouldBe a [java.lang.Double]
+    sum shouldBe a[HCons[_, HCons[_, HCons[_, HNil]]]]
+    sum.head shouldBe a[java.lang.Double]
     sum.head shouldBe 2.0
     sum.tail.head shouldBe "hi"
+    sum.tail.tail.head shouldBe 1
 
   }
+
+  /* FIXME
+  "HList + " should "append element to a hlist " in {
+
+    val hlist = 2.0 :: "hi" :: HNil
+    val sum = hlist + 1
+    sum shouldBe 2.0 :: "hi" :: 1 :: HNil
+    sum shouldBe a[HCons[_, HCons[_, HCons[_, HNil]]]]
+    sum.head shouldBe a[java.lang.Double]
+    sum.head shouldBe 2.0
+    sum.tail.head shouldBe "hi"
+    sum.tail.tail.head shouldBe 1
+
+  }*/
 
   "HList fold" should "old over elements and produce a result" in {
 
@@ -47,14 +69,14 @@ class HListSpec extends FlatSpec with Matchers {
   "HList filter" should "keep only elements satisfying predicate" in {
 
     //first element : false
-    (2.0 :: "hi" :: HNil).filter {
+    (2.0 :: "hi" :: "bye" :: HNil).filter {
       case s: String if s.startsWith("h") => true
       case _ => false
     } shouldBe ("hi" :: HNil)
 
 
     //first element : true
-    ("hi" :: 2.0 :: HNil).filter {
+    ("hi" :: 2.0 :: "bye" :: HNil).filter {
       case s: String if s.startsWith("h") => true
       case _ => false
     } shouldBe ("hi" :: HNil)
@@ -74,11 +96,27 @@ class HListSpec extends FlatSpec with Matchers {
 
   }
 
-
   "HList foreach" should "iterate over elements" in {
 
     val list = ListBuffer[String]()
     (2.0 :: "hi" :: "hello" :: HNil).foreach {
+      case s: String => list += s
+      case _ =>
+    }
+
+    list shouldBe List("hi", "hello")
+
+  }
+
+  "HList" should "chain map filter and foreach" in {
+
+    val list = ListBuffer[String]()
+    val hlist = 2.0 :: "bye" :: "hi" :: "hello" :: HNil
+
+    hlist.map(_.toString).filter {
+      case s: String if s.startsWith("h") => true
+      case _ => false
+    }.foreach {
       case s: String => list += s
       case _ =>
     }
--- src/test/scala/MonadTransformersSpec.scala
+++ src/test/scala/MonadTransformersSpec.scala
@@ -1,14 +1,16 @@
+import io.github.hamsters.Validation._
 import io.github.hamsters.{FutureEither, FutureOption}
 import org.scalatest._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class MonadTransformersSpec extends FlatSpec with Matchers {
 
   "FutureOption" should "handle Future[Option[_]] type" in {
     def foa: Future[Option[String]] = Future(Some("a"))
-    def fob(a: String): Future[Option[String]] = Future(Some(a+"b"))
+    def fob(a: String): Future[Option[String]] = Future(Some(a + "b"))
 
     val composedAB: Future[Option[String]] = (for {
       a <- FutureOption(foa)
@@ -29,7 +31,7 @@ class MonadTransformersSpec extends FlatSpec with Matchers {
       ab <- FutureOption(fob(a))
     } yield ab).future
 
-    an [Exception] should be thrownBy Await.result(composedABWithFailure, 1 second)
+    an[Exception] should be thrownBy Await.result(composedABWithFailure, 1 second)
   }
 
 
@@ -40,7 +42,7 @@ class MonadTransformersSpec extends FlatSpec with Matchers {
       (a, i) <- FutureOption(fo) if i > 5
     } yield a).future
 
-    Await.result(filtered, 1 second) shouldBe Some(("a"))
+    Await.result(filtered, 1 second) shouldBe Some("a")
 
     val filtered2 = (for {
       (a, i) <- FutureOption(fo) if i > 50
@@ -50,44 +52,44 @@ class MonadTransformersSpec extends FlatSpec with Matchers {
   }
 
   "FutureEither" should "handle Future[Either[_,_]] type" in {
-    def fea: Future[Either[String, Int]] = Future(Right(1))
-    def feb(a: Int): Future[Either[String, Int]] = Future(Right(a+2))
+    def fea: Future[Either[String, Int]] = Future(OK(1))
+    def feb(a: Int): Future[Either[String, Int]] = Future(OK(a + 2))
 
     val composedAB: Future[Either[String, Int]] = (for {
       a <- FutureEither(fea)
       ab <- FutureEither(feb(a))
     } yield ab).future
 
-    Await.result(composedAB, 1 second) shouldBe Right(3)
+    Await.result(composedAB, 1 second) shouldBe OK(3)
 
     val composedABWithNone: Future[Either[String, Int]] = (for {
-      a <- FutureEither(Future.successful(Left("d'oh!")))
+      a <- FutureEither(Future.successful(KO("d'oh!")))
       ab <- FutureEither(feb(a))
     } yield ab).future
 
-    Await.result(composedABWithNone, 1 second) shouldBe Left("d'oh!")
+    Await.result(composedABWithNone, 1 second) shouldBe KO("d'oh!")
 
     val composedABWithFailure: Future[Either[String, Int]] = (for {
       a <- FutureEither(Future.failed(new Exception("d'oh!")))
       ab <- FutureEither(feb(a))
     } yield ab).future
 
-    an [Exception] should be thrownBy Await.result(composedABWithFailure, 1 second)
+    an[Exception] should be thrownBy Await.result(composedABWithFailure, 1 second)
   }
 
   "FutureEither" should "be filtered with pattern matching in for comprehension" in {
-    def fe: Future[Either[String, (String, Int)]] = Future(Right(("a", 42)))
+    def fe: Future[Either[String, (String, Int)]] = Future(OK(("a", 42)))
 
     val filtered = (for {
       (a, i) <- FutureEither(fe) if i > 5
     } yield a).future
 
-    Await.result(filtered, 1 second) shouldBe Right("a")
+    Await.result(filtered, 1 second) shouldBe OK("a")
 
     val filtered2 = (for {
       (a, i) <- FutureEither(fe) if i > 50
     } yield a).future
 
-    Await.result(filtered2, 1 second) shouldBe Left("No value matching predicate")
+    Await.result(filtered2, 1 second) shouldBe KO("No value matching predicate")
   }
 }
